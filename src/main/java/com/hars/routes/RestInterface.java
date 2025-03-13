f03495a8c017578ba41a2b78a25e064f4b82421a
AverageDeveloperOhio <65859573+TheMastro-11@users.noreply.github.com>
2025-03-13 18:16:59 +0100

    Feat: Added Login

--- src/main/java/com/hars/routes/RestInterface.java
+++ src/main/java/com/hars/routes/RestInterface.java
@@ -3,7 +3,6 @@ package com.hars.routes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,10 +27,14 @@ public class RestInterface {
 		return "";
 	}
 
-	@CrossOrigin(origins = "http://127.0.0.1:49784")
 	@PostMapping("/RegisterUser")
 	public String registerUser(@RequestBody User user){
 		return userService.registerUser(user);
+	}
+
+	@PostMapping("/LoginUser")
+	public String loginUser(@RequestBody User user){
+		return userService.loginUser(user);
 	}
 
 
--- src/main/java/com/hars/services/UserService.java
+++ src/main/java/com/hars/services/UserService.java
@@ -1,5 +1,9 @@
 package com.hars.services;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +17,46 @@ public class UserService{
     private UserRepository userRepository;
 
     public String registerUser(User user){
-        User user2 = userRepository.save(user);
+        try {
+            User user2 = userRepository.save(user);
+            return "\"ok\"";
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
         return "\"no\"";
     
+    }
+
+    public String loginUser(User userLog){
+        Optional<User> userOpt = userRepository.findByName(userLog.getName());
+
+        if (userOpt.isEmpty()) {
+            return "Utente non trovato";
+        }
+
+        User user = userOpt.get();
+
+        String hashedPassword = hashPassword(userLog.getPassword());
+        if (!user.getPassword().equals(hashedPassword)) {
+            return "Password errata";
+        }
+ 
+        return "Login riuscito!"; 
+
+    }
+
+    private String hashPassword(String password) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] encodedHash = digest.digest(password.getBytes());
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : encodedHash) {
+                hexString.append(String.format("%02x", b));
+            }
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Errore nell'hashing della password", e);
+        }
     }
 }
