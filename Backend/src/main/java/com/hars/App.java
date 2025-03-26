c9b0197b8588284b4c0e33dac0be73942666d08c
AverageDeveloperOhio <65859573+TheMastro-11@users.noreply.github.com>
2025-03-26 12:13:37 +0100

    Fix: jparepository

--- Backend/src/main/java/com/hars/App.java
+++ Backend/src/main/java/com/hars/App.java
@@ -2,10 +2,14 @@ package com.hars;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.hars")
+@EntityScan("com.hars.persistence.entities")
+@EnableJpaRepositories("com.hars.persistence.repository")
 public class App {
 	
 	public static void main(String[] args) {
