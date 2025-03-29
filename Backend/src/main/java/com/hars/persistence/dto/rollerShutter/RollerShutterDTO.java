540c35e3a13e9df808ee79efc0f1ce295ebb14e7
AverageDeveloperOhio <65859573+TheMastro-11@users.noreply.github.com>
2025-03-29 20:19:34 +0100

    Refactor: Modified DTO

--- Backend/src/main/java/com/hars/persistence/dto/rollerShutter/RollerShutterDTO.java
+++ Backend/src/main/java/com/hars/persistence/dto/rollerShutter/RollerShutterDTO.java
@@ -1,21 +1,44 @@
 package com.hars.persistence.dto.rollerShutter;
+import com.hars.persistence.dto.home.HomeDTO;
 
 public class RollerShutterDTO {
+    private Long id;
     private String name;
-    private Integer percentageOpening;
+    private int percentageOpening;
+    private HomeDTO home;
 
     //Getters
+    public Long getId(){
+        return this.id;
+    }
+
     public String getName() {
         return this.name;
     }
 
+    public int getPercentageOpening(){
+        return this.percentageOpening;
+    }
+
+    public HomeDTO getHome(){
+        return this.home;
+    }
+
     //Setters
+    public void setId(Long id){
+        this.id = id;
+    }
+
     public void setName(String name){
         this.name = name;
     }
 
     public void setPercentageOpening(int percentageOpening){
         this.percentageOpening = percentageOpening;
+    }
+
+    public void setHome(HomeDTO home){
+        this.home = home;
     }
 
 }
