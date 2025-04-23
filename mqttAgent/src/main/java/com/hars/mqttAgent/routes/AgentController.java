f5a9a9019a0c9e7664f2c6877989377bb430135a
AverageDeveloperOhio <65859573+TheMastro-11@users.noreply.github.com>
2025-04-23 10:59:15 +0200

    Chore: Modified Name

---
+++ mqttAgent/src/main/java/com/hars/mqttAgent/routes/AgentController.java
@@ -0,0 +1,10 @@
+package com.hars.mqttAgent.routes;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/agent")
+public class AgentController {
+
+}
--- mqttAgent/src/main/java/com/hars/mqttAgent/routes/rest/RestController.java
+++
@@ -1,8 +0,0 @@
-package com.hars.mqttAgent.routes.rest;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class RestController {
-    
-}
